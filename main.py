f6f50d62c26c47498f81e0da24d359d4704c49a4
trporter <torri.porter@yahoo.com>
2019-02-19 15:19:13 -0800

    ready to add games

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ dice = range(dice_sides)
 def roll_dice():
     return random.choice(dice)
 
-def dice_game():
+def game1():
+    return 0
+
+def game2():
+    return 0
+
+def game3():
     return 0
 
 def interface():
     print "1: roll dice once"
-    print "2: play dice game"
+    print "2: play dice games"
     print "3: roll dice x times"
     print "4: exit"
 
@@ -20,7 +26,24 @@ def interface():
         if choice == 1:
             print roll_dice()
         elif choice == 2:
-            dice_game()
+            print "1: game1"
+            print "2: game2"
+            print "3: game3"
+            print "4: back"
+            game = input("what game?: ")
+
+            if game == 1:
+                game1()
+            elif game == 2:
+                game2()
+            elif game == 3:
+                game3()
+            elif game == 4:
+                interface()
+            else:
+                print "invalid choice"
+                interface()
+
         elif choice == 3:
             action = input("number of rolls: ")
             for i in range(action):
