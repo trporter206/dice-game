0712b40cf01c26a147e23f2d9e10a678b5ce7345
trporter <torri.porter@yahoo.com>
2019-02-19 14:39:03 -0800

    dice sides variable

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 
-dice = [1,2,3,4,5,6]
+dice_sides = 6
+dice = range(dice_sides)
 
 def roll_dice():
     return random.choice(dice)
+
+def dice_game():
+    return 0
 
 def interface():
     print "1: roll dice once"
@@ -16,7 +20,7 @@ def interface():
         if choice == 1:
             print roll_dice()
         elif choice == 2:
-            return 0
+            dice_game()
         elif choice == 3:
             action = input("number of rolls: ")
             for i in range(action):
