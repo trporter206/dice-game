2321a22860d52d29c3a5048b48d7c37b7dedfeaa
trporter <torri.porter@yahoo.com>
2019-02-24 23:44:08 -0800

    started 21 game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ dice = range(1,dice_sides)
 def roll_dice():
     return random.choice(dice)
 
-def first_to_winner(scores, target):
+def first_to_helper(scores, target):
     print scores
     diffs = [0]*len(scores)
     for index, s in enumerate(scores):
@@ -31,11 +31,19 @@ def first_to(players,target,max_rounds=50):
             scores[index-1]+= roll_dice()
             print('player '+str(index)+' score: '+str(s))
         if any(i >= target for i in scores):
-            first_to_winner(scores, target)
+            first_to_helper(scores, target)
             break
 
-def game2():
-    return 0
+def twenty_one_helper():
+    return 45
+
+def twenty_one(players):
+    scores = [0]*players
+    for i in range(1,players):
+        scores[i-1] = twenty_one_helper()
+        print('player '+str(i)+' score: '+str(scores[i-1]))
+    print('player '+str(scores.index(min(x for x in scores if x is not None))+1)+' wins!'+'\n')
+    interface()
 
 def game3():
     return 0
@@ -53,7 +61,7 @@ def interface():
             interface()
         elif choice == 2:
             print "1: first to"
-            print "2: game2"
+            print "2: 21"
             print "3: game3"
             print "4: back"
             game = input("what game?: ")
@@ -63,7 +71,8 @@ def interface():
                 target = input("target number: ")
                 first_to(players,target)
             elif game == 2:
-                game2()
+                players = input("number of players: ")
+                twenty_one(players)
             elif game == 3:
                 game3()
             elif game == 4:
@@ -79,7 +88,7 @@ def interface():
         elif choice == 4:
             exit()
         else:
-            print "invalid choice"
+            print("invalid choice"+'\n')
             interface()
     choose()
 
